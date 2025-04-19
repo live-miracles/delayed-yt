e1988710e43d9a5f936cf7ff73a826cbb8941c0e
Nandisha <197202821+nandisha0@users.noreply.github.com>
2025-04-19 16:39:01 +0000

    improve ui

--- stats.js
+++ stats.js
@@ -11,13 +11,10 @@ async function fetchStats() {
 
     try {
         const response = await fetch(statsServer);
-
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
         const stats = await response.json();
-
         return { stats: stats, error: null }; // Return full data if needed
     } catch (error) {
         return { stats: [], error: String(error) };
@@ -25,26 +22,20 @@ async function fetchStats() {
 }
 
 function getBoxHtml(status) {
-    let p1 = status.title;
-    let p2 = `${status.id} <span class="text-secondary">|</span> ${status.ip}`;
-    let p3 = durationToString(status.delay);
-    let p4 = durationToString(status.duration);
+    let delay = durationToString(status.delay);
+    let duration = durationToString(status.duration);
 
-    if (status.error) {
-        p1 = status.error;
-        p2 = '';
-        p3 = 'Error';
-        p4 = '';
-    }
+    const err = status.error;
     return `
-        <div class="w-fit min-w-[450px] max-w-[700px] h-fit text-center bg-neutral rounded-lg shadow-md m-2 p-3">
-            <p class="text-3xl">${p2}</p>
-            <p>
-                <span class="text-7xl ${status.error ? 'text-error' : ''}">${p3}</span>
-                <span class="text-3xl ml-3">${p4}</span>
+        <div class="w-fit min-w-[150px] max-w-[300px] h-fit text-center bg-neutral rounded-lg shadow-md m-1 p-1">
+            <p class="text-xl font-semibold my-1">
+                <span class="text-secondary">${err ? '' : status.title}</span>
+                <span class="${err ? 'text-error' : ''}">${err ? 'Error' : delay}</span>
             </p>
-            <p class="font-semibold text-3xl text-secondary my-1">${p1}</p>
-
+            <span class="text-sm">${err ? '' : duration}</span>
+            <span class="text-secondary">|</span>
+            <span class="text-sm">${err ? '' : status.id}</span>
+            <p class="text-sm">${err ? err : status.ip}</p>
         </div>`;
 }
 
